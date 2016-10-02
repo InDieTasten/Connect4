92e483afccf2f148f4e65d317b8093d9df5900fe
InDieTasten <indietasten@gmail.com>
2016-10-02 19:46:47 +0200

    Add logic for client-server connection establishment

--- Connect4_Client/main.cpp
+++ Connect4_Client/main.cpp
@@ -1,9 +1,95 @@
 #include <iostream>
 
 #include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
 
 int main()
 {
+
+	std::cout << "[Info] Connect4 Client launched" << std::endl;
+
+	while (true)
+	{
+
+		std::cout << "Insert host to play on: ";
+
+		std::string insertedHoststring;
+
+		std::cin >> insertedHoststring;
+
+		std::cout << "[Info] Trying to connect to " << insertedHoststring << std::endl;
+
+		sf::TcpSocket socket;
+
+		sf::Socket::Status status = socket.connect(insertedHoststring, 1337);
+
+		if (status != sf::Socket::Done)
+		{
+			std::cerr << "[Error] Unable to connect to " << insertedHoststring << std::endl;
+		}
+		else
+		{
+			std::cout << "[Info] Successfully connected to " << insertedHoststring << std::endl;
+			
+			int player;
+
+			std::cout << "[Info] Retrieving player info..." << std::endl;
+			sf::Packet packet;
+			if (socket.receive(packet) != sf::Socket::Done)
+			{
+				std::cerr << "[Error] Couldn't retrieve player info" << std::endl;
+			}
+			else
+			{
+				int code;
+				packet >> code;
+				std::string message;
+
+				switch (code)
+				{
+				case 201: //waiting packet, meaning this client is player 0
+					packet >> message;
+					std::cout << message << std::endl;
+					player = 0;
+					break;
+				case 202: //player info packet, meaning this client in player 1
+					packet >> message;
+					std::cout << message << std::endl;
+					player = 1;
+					break;
+				default:
+					std::cerr << "[Error] Unexpected status code: " << code << std::endl;
+					continue;
+				}
+
+				sf::Packet gamestartPacket;
+				if (socket.receive(gamestartPacket) != sf::Socket::Done)
+				{
+					std::cerr << "[Error] Couldn't receive game start notification" << std::endl;
+				}
+				else
+				{
+					int code;
+					gamestartPacket >> code;
+
+					if (code == 300)
+					{
+						std::cout << "[Info] Game started!" << std::endl;
+						std::cout << "You are player #" << player << std::endl;
+						//
+						sf::sleep(sf::seconds(10.0f));
+					}
+					else
+					{
+						std::cerr << "[Error] Unexpected status code: " << code << std::endl;
+					}
+				}
+			}
+
+
+		}
+	}
+
 
 	return EXIT_SUCCESS;
 }
--- Connect4_Server/main.cpp
+++ Connect4_Server/main.cpp
@@ -1,8 +1,84 @@
 #include <iostream>
 #include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
 
 int main()
 {
+
+	std::cout << "[Info] Connect4 Server launched" << std::endl;
+
+	while (true)
+	{
+		std::cout << "[Info] Waiting for players..." << std::endl;
+
+		sf::TcpListener listener;
+
+		if (listener.listen(1337) != sf::Socket::Done)
+		{
+			std::cerr << "[Error] Oh noes!! An error occured" << std::endl;
+		}
+		else
+		{
+			sf::TcpSocket player0;
+
+			if (listener.accept(player0) != sf::Socket::Done)
+			{
+				std::cerr << "[Error] Oh noes!! An error occured" << std::endl;
+			}
+			else
+			{
+				std::cout << "[Info] Player #0 connected. Waiting for opponent..." << std::endl;
+				sf::Packet player0WaitingPacket;
+				player0WaitingPacket << 201 << "[Info] Waiting for opponent...";
+				if (player0.send(player0WaitingPacket) != sf::Socket::Done)
+				{
+					std::cerr << "[Error] Player #0 couldn't receive waiting packet" << std::endl;
+				}
+				else
+				{
+					sf::TcpSocket player1;
+
+					if (listener.accept(player1) != sf::Socket::Done)
+					{
+						std::cerr << "[Error] Oh noes!! An error occured" << std::endl;
+					}
+					else
+					{
+						std::cout << "[Info] Player #1 connected!" << std::endl;
+						sf::Packet player1playerInfo;
+						player1playerInfo << 202 << "[Info] Player #0 already awaited you";
+						if (player1.send(player1playerInfo) != sf::Socket::Done)
+						{
+							std::cerr << "[Error] Player #1 couldn't receive player info packet" << std::endl;
+						}
+						else
+						{
+							sf::Packet gamestartedPacket;
+							gamestartedPacket << 300;
+
+							if (player0.send(gamestartedPacket) != sf::Socket::Done)
+							{
+								std::cerr << "[Error] Player #0 couldn't receive game start notification" << std::endl;
+							}
+							else
+							{
+								if (player1.send(gamestartedPacket) != sf::Socket::Done)
+								{
+									std::cerr << "[Error] Player #1 couldn't receive game start notification" << std::endl;
+								}
+								else
+								{
+									//
+									sf::sleep(sf::seconds(10.0f));
+								}
+
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
